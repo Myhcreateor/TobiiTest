2e15330dfe33655ef6ec5af429a6d74ce7340d96
Myhcreateor <995218505@qq.com>
2021-10-25 16:18:33 +0800

    完成开场动画结束自动跳转游戏主界面

--- Assets/Script/StartAnimation.cs
+++ Assets/Script/StartAnimation.cs
@@ -6,12 +6,16 @@ using UnityEngine.Video;
 public class StartAnimation : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
+    private double time;
+    private double timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
+        StartCoroutine(PlayVideo());
     }
-     void Update()
+
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -19,4 +23,14 @@ public class StartAnimation : MonoBehaviour
         }
     }
 
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.loopPointReached += EndVideo;
+        yield return null;
+    }
+    public void EndVideo(VideoPlayer videoPlayer)
+    {
+        Debug.Log("视频播放结束");
+        Application.LoadLevel(1);
+    }
 }
