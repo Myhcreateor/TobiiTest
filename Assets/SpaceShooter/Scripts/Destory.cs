2ce7394deba67a39651e9dde2630952b7515106a
Myhcreateor <995218505@qq.com>
2021-10-25 18:37:32 +0800

    给飞机添加注视后爆炸的方法，并完成数据更新

--- Assets/SpaceShooter/Scripts/Destory.cs
+++ Assets/SpaceShooter/Scripts/Destory.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using DG.Tweening;
 using Tobii.Gaming;
-using DG.Tweening;
-using System;
+using UnityEngine;
 
-public class Destory : MonoBehaviour {
+public class Destory : MonoBehaviour
+{
 
-	public GameObject explosion;
-	public GameObject explosionplayer;
-	private int value;
-    public float deadTime=1.2f;
-    private float deadTimer=0.0f;
-    private bool isTranslate=false;
+    public GameObject explosion;
+    public GameObject explosionplayer;
+    private int value;
+    public float deadTime = 1.2f;
+    private float deadTimer = 0.0f;
+    private bool isTranslate = false;
     private GameObject prego;
-	private GameController gameController;
-	void Start(){
+    private GameController gameController;
+    void Start()
+    {
         gameController = GameController.instance;
         value = gameController.gameInfromarion.value;
     }
@@ -30,21 +29,42 @@ public class Destory : MonoBehaviour {
             {
                 Tween twe = go.transform.DOScale(new Vector3(1.25f, 1.25f, 1.25f), 1.2f);
                 twe.SetEase(Ease.InCubic);
-                twe.OnComplete(()=>
+                twe.OnComplete(() =>
                 {
                     Instantiate(explosion, go.transform.position, go.transform.rotation);
                     DestroyImmediate(go.gameObject);
                     gameController.addScore(value);
                     GameProgress.finished++;
                     GameProgress.continuousFinished++;
-                    GameProgress.continuousUnfinished=0;
+                    GameProgress.continuousUnfinished = 0;
                     GameProgress.AdjustDifficult();
-                    Debug.Log("differentLevel:"+GameProgress.differentLevel);
+                    Debug.Log("differentLevel:" + GameProgress.differentLevel);
+                });
+                isTranslate = true;
+            }
+        }
+        else if (go != null && go.tag == "Aircaft")
+        {
+            go.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            if (isTranslate == false)
+            {
+                Tween twe = go.transform.DOScale(new Vector3(1.05f, 1.05f, 1.05f), 1.2f);
+                twe.SetEase(Ease.InCubic);
+                twe.OnComplete(() =>
+                {
+                    Instantiate(explosionplayer, go.transform.position, go.transform.rotation);
+                    DestroyImmediate(go.gameObject);
+                    gameController.addScore(-value);
+                    //GameProgress.finished++;
+                    //GameProgress.continuousFinished++;
+                    //GameProgress.continuousUnfinished = 0;
+                    //GameProgress.AdjustDifficult();
+                    //Debug.Log("differentLevel:" + GameProgress.differentLevel);
                 });
                 isTranslate = true;
             }
         }
     }
-
-
 }
+
+
